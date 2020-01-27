68c0b1ffe50a5778c81c3c7abc996ceaec0834b6
Ying Huang <huangyingw@gmail.com>
2020-01-26 21:53:47 -0800

    n

--- mysql-wordpress-pd/run.sh
+++ mysql-wordpress-pd/run.sh
@@ -3,6 +3,5 @@ SCRIPT=$(realpath "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 cd "$SCRIPTPATH"
 
-kubectl taint nodes --all node-role.kubernetes.io/master-
 kubectl apply -f local-volumes.yaml
 kubectl apply -k ./
