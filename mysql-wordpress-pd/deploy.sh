55ae3edd3191bb8ff0b3fde3245eb635ceed1548
Ying Huang <huangyingw@gmail.com>
2020-06-22 23:24:40 -0400

    n

--- mysql-wordpress-pd/deploy.sh
+++ mysql-wordpress-pd/deploy.sh
@@ -3,5 +3,6 @@ SCRIPT=$(realpath "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 cd "$SCRIPTPATH"
 
+# https://kubernetes.io/docs/tutorials/stateful-application/mysql-wordpress-persistent-volume/
 kubectl apply -f local-volumes.yaml
 kubectl apply -k ./
