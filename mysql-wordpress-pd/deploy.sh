e7369ebe30aa6f1d81eb48b1110eae934a6e62a8
Ying Huang <huangyingw@gmail.com>
2020-06-25 00:30:09 -0400

    n

--- mysql-wordpress-pd/deploy.sh
+++ mysql-wordpress-pd/deploy.sh
@@ -6,3 +6,4 @@ cd "$SCRIPTPATH"
 # https://kubernetes.io/docs/tutorials/stateful-application/mysql-wordpress-persistent-volume/
 kubectl apply -f local-volumes.yaml
 kubectl apply -k ./
+kubectl patch svc wordpress -p '{"spec": {"type": "LoadBalancer", "externalIPs":["10.1.10.168"]}}'
